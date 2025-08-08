acf8053960395ad647dac84c8fa41ee67aba7943
Kawser <mdshakil4364@gmail.com>
2025-08-08 20:05:14 +0600

    prisma added and 51:00 video

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,13 @@
 import NextAuth from "next-auth"
+ import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+import authConfig from "./auth.config";
+
+const prisma = new PrismaClient()
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
