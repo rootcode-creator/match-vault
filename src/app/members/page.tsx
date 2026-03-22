e065203cbfc14dd9b80d94cf8279f35222d41fcc
Kawser Ahmad <bestpdf4364@gmail.com>
2026-03-23 04:11:46 +0600

    Refactor members page rendering: streamline conditional rendering for member list and improve layout structure

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -13,28 +13,25 @@ export default async function MembersPage({searchParams,}:{
   const {items:members, totalCount}= await getMembers(params);
   const likeIds = await fetchCurrentUserLikeIds();
 
-  if (!members?.length) {
-    return (
-      <div className="mt-6 mb-10 px-4 sm:px-6 lg:px-8">
-        <p className="text-default-500">No members found.</p>
-      </div>
-    );
-  }
-
   return (
     <div className="mb-10 px-4 sm:px-6 lg:px-8">
       <Filters />
 
-      <div className="mt-6 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
-       {members &&
-          members.map((member) => (
+      {!members?.length ? (
+        <div className="mt-6">
+          <p className="text-default-500">No members found.</p>
+        </div>
+      ) : (
+        <div className="mt-6 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+          {members.map((member) => (
             <MemberCard
               member={member}
               key={member.id}
               likeIds={likeIds}
             />
           ))}
-      </div>
+        </div>
+      )}
 
       <PaginationComponent />
     </div>
